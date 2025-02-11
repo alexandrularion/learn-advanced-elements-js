f4d2e4ae38f0a630cacdbd72d996fefbf082e430
alexandrularion <larion.a@yahoo.com>
2025-02-11 19:37:50 +0200

    Updated concepts for JavaScriptRO46

--- JavaScriptRO46/concepts/2_regex/index.js
+++ JavaScriptRO46/concepts/2_regex/index.js
@@ -33,3 +33,18 @@ const sentence2 =
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat natus sequi animi autem hic, error aspernatur saepe asperiores quia molestias eligendi delectus voluptas voluptatum, amet nam porro vel. Corrupti, ad.";
 
 console.log(sentence2.match(keyRegex)); // ["amet", "amet"]
+
+// Tip: Find and replace the phone number using a regex
+const sentence3 =
+  "Call me at 0723-456-789 or (021) 345 6789. My old number was 0040 723 456 789.";
+
+const replaceSentence3 = "[hidden number]";
+
+// \b(0\d{2}[-.\s]?\d{3}[-.\s]?\d{3})\b - 0723-456-789 | 0723.456.789 | 0723 456 789
+// \b(\(021\)\s?\d{3}[-.\s]?\d{4})\b - (021) 345 6789 | (021)345 6789
+// \b(0040\s?\d{3}[-.\s]?\d{3}[-.\s]?\d{3})\b - 0040 723 456 789 | 0040-723-456-789
+const replaceRegex =
+  /\b(0\d{3}[-.]?\d{3}[-.]?\d{3})\b|(\(021\)\s?\d{3}[-.\s]?\d{4})\b|\b(0040\s?\d{3}[-.\s]?\d{3}[-.\s]?\d{3})\b/g;
+
+const sentence3Replaced = sentence3.replace(replaceRegex, replaceSentence3);
+console.log(sentence3Replaced); // "Call me at [hidden number] or [hidden number]. My old number was [hidden number]."
