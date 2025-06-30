6c0bc48c646fe6e1b3ed298bdcc792824b183ab9
alexandrularion <larion.a@yahoo.com>
2025-06-30 21:28:05 +0300

    Updated concepts for JavaScriptRO48

---
+++ JavaScriptRO48/concepts/2-regex/index.js
@@ -0,0 +1,28 @@
+// Regular Expression
+// string = text
+
+// Regex - no rules
+const wordRegex = /milk/;
+
+const sentence = "We drink milk.";
+
+// 1. Check if the word "milk" is contained by our sentence
+const wordResult = wordRegex.test(sentence);
+
+console.log(wordResult); // true
+console.log(sentence.includes("milk")); // true
+
+// Regex - with rules
+const passwordRegex =
+  /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+// (?=.*[0-9]) - positive lookahead - at least one digit (number)
+// (?=.*[!@#$%^&*]) - positive lookahead - at least one special char
+// (?=.*[A-Z]) - positive lookahead - at least one uppercase letter
+// [a-zA-Z0-9!@#$%^&*] - any uppercase or lowercase letters, any digits, any special char
+// {6,16} - 6 - is the minimum length / 16 - is the maximum length
+
+const passwordSentence = "parolasecreta12*";
+
+const passwordResult = passwordRegex.test(passwordSentence);
+console.log("Password check: ", passwordResult); // false
